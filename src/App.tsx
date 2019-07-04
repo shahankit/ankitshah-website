7ddfb019541320ecbe305150342a7e1a01fb4b45
Ankit Shah <shahankit2313@gmail.com>
2019-07-04 11:13:21 +0530

    Add typewriter effect to text

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,23 @@
-import React from "react";
+import React, { Component } from "react";
 import "./App.css";
+import typewrite from "./helpers/typewrite";
 
-const App: React.FC = () => {
-  return (
-    <div className="app">
-      <div className="title">Ankit Shah</div>
-    </div>
-  );
-};
+class App extends Component {
+  titleNode = React.createRef<HTMLSpanElement>();
+
+  componentDidMount() {
+    typewrite(this.titleNode.current!, "Ankit Shah");
+  }
+
+  render() {
+    return (
+      <div className="app">
+        <div className="title-container">
+          <span ref={this.titleNode} className="title" />
+        </div>
+      </div>
+    );
+  }
+}
 
 export default App;
---
+++ src/helpers/typewrite.ts
@@ -0,0 +1,17 @@
+export default function typewrite(node: HTMLElement, text: string) {
+  let renderedTextLength = 0;
+  node.classList.add("cursor-animation");
+  function animate() {
+    renderedTextLength += 1;
+    node.textContent = text.substr(0, renderedTextLength);
+
+    if (renderedTextLength !== text.length) {
+      const delta = 100 + Math.random() * 100;
+      setTimeout(() => window.requestAnimationFrame(animate), delta);
+    } else {
+      node.classList.remove("cursor-animation");
+    }
+  }
+
+  setTimeout(() => window.requestAnimationFrame(animate), 500);
+}
